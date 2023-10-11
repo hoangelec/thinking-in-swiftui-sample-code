26e233caa5c3ecd4ba924d6d02b8d7f1ae5072aa
Mike Cap <hoangelec@icloud.com>
2023-10-11 19:29:11 +0700

    Added experiment for HStack priority

--- Self Experiments/SwiftUILayoutExperiments/SwiftUILayoutExperiments/MeasureBehavior.swift
+++ Self Experiments/SwiftUILayoutExperiments/SwiftUILayoutExperiments/MeasureBehavior.swift
@@ -20,32 +20,36 @@ struct MeasureBehavior<Content: View>: View {
     var body: some View {
         VStack {
             content
-                .border(Color.green)
+                .border(Color.green, width: 0.5)
                 .frame(width: width, height: height)
                 .border(Color.black)
                 .frame(width: UIScreen.main.bounds.width, height:  501)
                 .background(Color.blue)
             HStack {
-                Text("Width")
-                Slider(value: $width, in: 0...200)
+                Text("Width \(Int(width))")
+                Slider(value: $width, in: 0...300)
             }
             HStack {
                 Text("Height")
                 Slider(value: $height, in: 0...500)
             }
-
         }
+
     }
 }
 
 struct MeasureBehavior_Previews: PreviewProvider {
     static var previews: some View {
         MeasureBehavior(
-            content:
-                Text("This is a b c d e f g h i j k l ")
-                .frame(idealWidth: 80)
-                .fixedSize()
-
+            content: {
+                HStack {
+                    Text("Hello, World")
+                        .fixedSize()
+                        .layoutPriority(2)
+                    Rectangle().fill(Color.red)
+//                        .frame(minWidth: 50)
+                }
+            }()
         )
         .padding()
     }
